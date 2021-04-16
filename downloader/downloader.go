ee5ecb6cffff420c4b1361ccb44b4ffd38a52e50
Nathan13888 <29968201+Nathan13888@users.noreply.github.com>
2021-04-16 16:18:12 -0400

    support M3U8
    Signed-off-by: Nathan13888 <29968201+Nathan13888@users.noreply.github.com>

--- downloader/downloader.go
+++ downloader/downloader.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"strings"
 	"time"
 
 	"dcs/config"
 
+	"github.com/Nathan13888/m3u8/dl"
 	"github.com/cavaliercoder/grab"
 	"github.com/cheggaaa/pb/v3"
 )
@@ -45,13 +47,35 @@ func Get(info DownloadInfo) error {
 	// TODO: more advanced lookup and incorporate checksums
 
 	setupTime := time.Since(start)
+	downloadStart := time.Now()
 
 	// TODO: look up if target file exists and show prompt; accept flags
 
+	fmt.Printf("Downloading '%v'\n\n", info.Link)
+	if strings.HasSuffix(info.Link, ".mp4") {
+		err = DownloadMP4(info, path, partPath)
+	} else if strings.HasSuffix(info.Link, ".m3u8") {
+		err = DownloadM3U8(info.Link, path)
+	} else {
+		return errors.New("unsupported file ending from '" + info.Link + "'")
+	}
+	if err != nil {
+		return err
+	}
+
+	downloadTime := time.Since(downloadStart)
+	// TODO: Scrap Time
+	fmt.Printf("* Setup Time      >> %v\n", setupTime)
+	fmt.Printf("* Download Time   >> %v\n", downloadTime)
+
+	return nil
+}
+
+func DownloadMP4(info DownloadInfo, path string, partPath string) error {
+	var err error
 	// Start downloading
 	client := grab.NewClient()
 	req, _ := grab.NewRequest(partPath, info.Link)
-	fmt.Printf("Downloading '%v'\n\n", req.URL())
 	res := client.Do(req)
 	fmt.Printf("Response: %v\n\n", res.HTTPResponse.Status)
 
@@ -82,18 +106,43 @@ Loop:
 		return err
 	}
 
-	fmt.Printf("\nDownload completed. Renaming file to final name.\n\n")
-	err = os.Rename(path+".part", path)
+	fmt.Printf("\nDownload completed. Renaming file to final destination.\n\n")
+	err = os.Rename(partPath, path)
 	if err != nil {
 		return err
 	}
 
-	downloadTime := time.Since(res.Start)
-	// TODO: Scrap Time
-	fmt.Printf("* Setup Time      >> %v\n", setupTime)
-	fmt.Printf("* Download Time   >> %v\n", downloadTime)
-
 	return nil
+}
+
+func DownloadM3U8(url string, p string) error {
+	var err error
+
+	streams := 4 // number of concurrent downloaders
+	tmpPath := p + "_m3u8files"
+	mergedFile := path.Join(tmpPath, "merged.ts")
+
+	downloader, err := dl.NewTask(tmpPath, url)
+	if err != nil {
+		return err
+	}
+	if err := downloader.Start(streams); err != nil {
+		return err
+	}
+
+	fmt.Printf("\nFinished merging M3U8 files. Renaming file to final destination.\n\n")
+	err = os.Rename(mergedFile, p)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Cleaning up... Removing '%s'\n\n", tmpPath)
+	err = os.RemoveAll(tmpPath)
+	if err != nil {
+		return err
+	}
+
+	return err
 }
 
 // Lookup - Check if downloaded content exists
