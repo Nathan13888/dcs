ec6b1b0da855f6576ec112f654e13a8b76e6f89f
Nathan13888 <29968201+Nathan13888@users.noreply.github.com>
2021-05-10 14:26:15 -0400

    server: implement better logging, graceful shutdown
    Signed-off-by: Nathan13888 <29968201+Nathan13888@users.noreply.github.com>

--- daemon/server.go
+++ daemon/server.go
@@ -1,22 +1,29 @@
 package daemon
 
 import (
+	"context"
 	"dcs/config"
 	"fmt"
-	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/rs/zerolog/log"
 )
 
 // StartTime - start time of the daemon
 var StartTime time.Time
+var DEBUGMODE = false
 
 // Start - start a HTTP API service
-func Start() {
+func Start(debug bool) {
+	if debug {
+		DEBUGMODE = true
+	}
+
 	_, port := config.DaemonURL()
-	log.Printf("Starting DCS Daemon at port %d\n\n", port)
 
 	r := mux.NewRouter()
 	server := &http.Server{
@@ -27,14 +34,48 @@ func Start() {
 		IdleTimeout:  60 * time.Second,
 	}
 
-	r.HandleFunc("/", handlePing).Methods("GET")
-	r.HandleFunc("/ping", handlePing).Methods("GET")
-	r.HandleFunc("/status", handlePing).Methods("GET")
+	r.HandleFunc("/", getStatus).Methods("GET")
+	r.HandleFunc("/ping", getStatus).Methods("GET")
+	r.HandleFunc("/status", getStatus).Methods("GET")
 	r.HandleFunc("/api/recentdownloads", getRecentDownloads).Methods("GET")
 	r.HandleFunc("/api/recentdownload", postRecentDownload).Methods("POST")
 	r.HandleFunc("/api/download", postDownload).Methods("POST")
 
+	r.Use(loggingMiddleware)
+	closeLogger := configureLogger()
+	defer closeLogger()
+
+	config.IS_SERVER = true
 	StartTime = time.Now()
 
-	log.Fatal(server.ListenAndServe())
+	log.Info().Msgf("Starting DCS Daemon at port %d", port)
+
+	// Run our server in a goroutine so that it doesn't block.
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Err(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, os.Interrupt)
+
+	// Block until we receive our signal.
+	<-c
+
+	wait := 15 * time.Second
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+	// Doesn't block if no connections, but will otherwise wait
+	// until the timeout deadline.
+	server.Shutdown(ctx)
+	// Optionally, you could run srv.Shutdown in a goroutine and block on
+	// <-ctx.Done() if your application should wait for other services
+	// to finalize based on context cancellation.
+	log.Info().Msg("shutting down...")
+	os.Exit(0)
 }
