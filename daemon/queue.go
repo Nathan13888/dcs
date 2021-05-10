1dd16e5de982c08c81ff90a9ebef9e4ce386f0dd
Nathan13888 <29968201+Nathan13888@users.noreply.github.com>
2021-05-10 14:30:21 -0400

    update server queue
    Signed-off-by: Nathan13888 <29968201+Nathan13888@users.noreply.github.com>

--- daemon/queue.go
+++ daemon/queue.go
@@ -1,12 +1,33 @@
 package daemon
 
-import "log"
+import (
+	"dcs/downloader"
+	"log"
+)
 
-var jobs = make(map[string]DownloadJob)
+var jobs = make(map[string]*DownloadJob)
 
 func AddJob(job DownloadJob) {
 	log.Printf("Recording new job with ID %s\n", job.ID)
 
-	jobs[job.ID] = job
-	// StartJob(job.ID)
+	jobs[job.ID] = &job
+}
+
+func StartJob(id string) {
+	if !JobExists(id) {
+		log.Printf("WARN: Job with ID %s cannot be found", id)
+		return
+	}
+	log.Printf("Starting job with ID %s\n", id)
+	job := jobs[id]
+	job.Status = RunningJob
+	go func() {
+		// jobLogger:=log.New()
+		downloader.Get(job.Req.DInfo, job.Req.Props)
+	}()
+}
+
+func JobExists(id string) bool {
+	_, ok := jobs[id]
+	return ok
 }
