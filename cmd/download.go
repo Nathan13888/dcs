1d055f57d9b64114cea161c528efbe862399a49a
Nathan13888 <29968201+Nathan13888@users.noreply.github.com>
2021-06-23 17:19:42 -0400

    remove useless comments
    Signed-off-by: Nathan13888 <29968201+Nathan13888@users.noreply.github.com>

--- cmd/download.go
+++ cmd/download.go
@@ -396,14 +396,4 @@ func init() {
 	downloadCmd.Flags().BoolP("bulk", "b", false, "bulk mode")
 	downloadCmd.Flags().BoolP("tryout", "t", false, "tryout a drama (just assume ep 1)")
 	downloadCmd.Flags().BoolP("manual", "M", false, "manual mode (use own download link)")
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// downloadCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// downloadCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cmd/service.go
+++ cmd/service.go
@@ -26,16 +26,6 @@ var serviceCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(serviceCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// serviceCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// serviceCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 func Request(method string, rpath string) (*http.Response, error) {
